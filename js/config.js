e41cb1b98d264a71807be7ae4d5446a8120cd1ae
Aldrin Padua <90054492+aldrinpadua@users.noreply.github.com>
2026-08-08 16:52:57 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,4 +12,5 @@ export const CONFIG = {
   MODE: "cloud", // "local" | "cloud"
   SUPABASE_URL: "https://lweuzfauuwqaueyctmmj.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_mA1m1Oc6AW0ZfSWkHdxYLQ_Ph_sHhHX",
+  TURNSTILE_SITE_KEY: "0x4AAAAAAEKiJTjylC-TWSJv",
 };
